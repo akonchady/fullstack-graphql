7224af51a8302088b795e872b97d42e95a15f054
Adarsh Konchady <akonchady@gmail.com>
2019-10-15 13:09:31 -0500

    useMutation for adding Pets

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -14,22 +14,52 @@ const ALL_PETS = gql`
     }
   }
 `;
+
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addedPet: addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 export default function Pets() {
   const [modal, setModal] = useState(false);
   const { data = {}, isLoading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet = {}] = useMutation(NEW_PET);
+
+  const { loading: isNewPetloading, data: dataMutation = {}, error: isNewPetError } = newPet;
+  const { addedPet } = dataMutation;
+
+  debugger;
+  // newPet -> {data, isMutationLoading, error}
+
   const { pets } = data;
   debugger;
 
-  if (isLoading) {
-    return <Loader/>
+  if (isLoading || isNewPetloading) {
+    return <Loader />;
   }
 
-  if (error) {
-    return <div>There was an error!</div>
+  if (error || isNewPetError) {
+    return <div>There was an error!</div>;
   }
 
   const onSubmit = input => {
     setModal(false);
+    const { type, name } = input;
+    createPet({
+      // pass variables
+      variables: {
+        newPet: {
+          type,
+          name
+        }
+      }
+    });
   };
 
   if (modal) {
